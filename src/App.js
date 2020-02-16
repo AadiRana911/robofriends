4953f2ba79d1a50dd4e366612f51def8864344de
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-02-16 20:20:35 +0500

    Getting users from json online server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
 import Searchbox from './searchbox';
 import CardList from './CardList';
-import {robots} from './robots'
+import './App.css';
 
 class App extends Component{
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
     }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
@@ -19,14 +24,17 @@ class App extends Component{
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
-        return(
-            <div className = 'tc'>
-                <h1>RoboFriends</h1>
-                <Searchbox searchChange = {this.onSearchChange} />
-                <CardList robots = {filteredRobots}/>
-            </div>
-            
-        );
+        if(this.state.robots.length === 0){
+            return <h1>LOADING</h1>
+        }else{
+            return(
+                <div className = 'tc'>
+                    <h1 className = 'f1'>RoboFriends</h1>
+                    <Searchbox searchChange = {this.onSearchChange} />
+                    <CardList robots = {filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
     return(
-        <div className = 'bg-light-green dib br3 pa3 ma2 grow shadow-5'>
+        <div className = 'tc grow bg-light-green dib br3 pa3 ma2 shadow-5'>
             <img alt = 'Robots' src = {`https://robohash.org/${id}?200x200`}/>
             <div>
                 <h2>{name}</h2>
